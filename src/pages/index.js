d5ebf326b577f0ec2f023a5a1a7919d85a66360e
Iye Szin Ang <isang@merimenkl.com>
2019-07-13 11:25:15 +0800

    add home page

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<div>
+			<h1>
+				Welcome to Iye Szin's Gatsby Site
+			</h1>
+			<p>
+				crafted with tears and love.
+			</p>
+		</div>
+	)
+}
+
+export default HomePage;
